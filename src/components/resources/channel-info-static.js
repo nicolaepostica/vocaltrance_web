0768f87f0c4ee6b5dff64d771a72fc40ea1d5c22
Nicolae Postica <nicolae.postica2@gmail.com>
2020-02-05 14:34:56 +0200

    [build][stable][version 1.6] full integrate https

--- src/components/resources/channel-info-static.js
+++ src/components/resources/channel-info-static.js
@@ -1,31 +1,31 @@
 export default {
   "radio": {
-    url: "http://vocaltrance.fm:8000/vocaltrance",
+    url: "https://vocaltrance.fm/radio/vocaltrance",
     channel_name: "Vocal Trance",
     block_name: "RADIO CHANNELS",
   },
   "vocaltrance": {
-    url: "http://vocaltrance.fm:8000/vocaltrance_",
+    url: "https://vocaltrance.fm/radio/vocaltrance_",
     channel_name: "Vocal Trance",
     block_name: "Vocal Trance",
   },
   "deep": {
-    url: "http://vocaltrance.fm:8000/deep_",
+    url: "https://vocaltrance.fm/radio/deep_",
     channel_name: "Deep Vocal House",
     block_name: "Deep Vocal House",
   },
   "positive": {
-    url: "http://vocaltrance.fm:8000/positive_",
+    url: "https://vocaltrance.fm/radio/positive_",
     channel_name: "Positive Radio",
     block_name: "Positive Radio",
   },
   "uplifting": {
-    url: "http://vocaltrance.fm:8000/uplifting_",
+    url: "https://vocaltrance.fm/radio/uplifting_",
     channel_name: "Uplifting Trance",
     block_name: "Uplifting Trance",
   },
   "chillout": {
-    url: "http://vocaltrance.fm:8000/vocalchillout_",
+    url: "https://vocaltrance.fm/radio/vocalchillout_",
     channel_name: "Vocal Chillout",
     block_name: "Vocal Chillout",
   }
--- src/components/resources/songs-data.js
+++ src/components/resources/songs-data.js
@@ -1,97 +1,97 @@
 export default [
   {
     key:  "vocaltrance",
-    url: 'http://176.9.36.203:8000/vocaltrance',
+    url: 'https://vocaltrance.fm/radio/vocaltrance',
     title: 'vocal trance',
     duration: 0
   },
   {
     key:  "vocaltrance",
-    url: 'http://176.9.36.203:8000/vocaltrance_128',
+    url: 'https://vocaltrance.fm/radio/vocaltrance_128',
     title: 'vocal trance 128',
     duration: 0
   },
   {
     key:  "vocaltrance",
-    url: 'http://176.9.36.203:8000/vocaltrance_320',
+    url: 'https://vocaltrance.fm/radio/vocaltrance_320',
     title: 'vocal trance 256',
     duration: 0
   },
   {
     key:  "vocaltrance",
-    url: 'http://176.9.36.203:8000/vocaltrance_256',
+    url: 'https://vocaltrance.fm/radio/vocaltrance_256',
     title: 'vocal trance 320',
     duration: 0
   },
   {
     key:  "deep",
-    url: 'http://176.9.36.203:8000/deep_128',
+    url: 'https://vocaltrance.fm/radio/deep_128',
     title: 'deep vocal house 128',
     duration: 0
   },
   {
     key:  "deep",
-    url: 'http://176.9.36.203:8000/deep_256',
+    url: 'https://vocaltrance.fm/radio/deep_256',
     title: 'deep vocal house 256',
     duration: 0
   },
   {
     key:  "deep",
-    url: 'http://176.9.36.203:8000/deep_320',
+    url: 'https://vocaltrance.fm/radio/deep_320',
     title: 'deep vocal house 320',
     duration: 0
   },
   {
     key:  "positive",
-    url: 'http://176.9.36.203:8000/positive_128',
+    url: 'https://vocaltrance.fm/radio/positive_128',
     title: 'positive house 128',
     duration: 0
   },
   {
     key:  "positive",
-    url: 'http://176.9.36.203:8000/positive_256',
+    url: 'https://vocaltrance.fm/radio/positive_256',
     title: 'positive house 256',
     duration: 0
   },
   {
     key:  "positive",
-    url: 'http://176.9.36.203:8000/positive_320',
+    url: 'https://vocaltrance.fm/radio/positive_320',
     title: 'positive house 320',
     duration: 0
   },
   {
     key:  "uplifting",
-    url: 'http://176.9.36.203:8000/uplifting_128',
+    url: 'https://vocaltrance.fm/radio/uplifting_128',
     title: 'uplifting trance 128',
     duration: 0
   },
   {
     key:  "uplifting",
-    url: 'http://176.9.36.203:8000/uplifting_256',
+    url: 'https://vocaltrance.fm/radio/uplifting_256',
     title: 'uplifting trance 256',
     duration: 0
   },
   {
     key:  "uplifting",
-    url: 'http://176.9.36.203:8000/uplifting_320',
+    url: 'https://vocaltrance.fm/radio/uplifting_320',
     title: 'uplifting trance 320',
     duration: 0
   },
   {
     key:  "chillout",
-    url: 'http://176.9.36.203:8000/vocalchillout_128',
+    url: 'https://vocaltrance.fm/radio/vocalchillout_128',
     title: 'vocal chillout 128',
     duration: 0
   },
   {
     key:  "chillout",
-    url: 'http://176.9.36.203:8000/vocalchillout_256',
+    url: 'https://vocaltrance.fm/radio/vocalchillout_256',
     title: 'vocal chillout 256',
     duration: 0
   },
   {
     key:  "chillout",
-    url: 'http://176.9.36.203:8000/vocalchillout_320',
+    url: 'https://vocaltrance.fm/radio/vocalchillout_320',
     title: 'vocal chillout 320',
     duration: 0
   }
