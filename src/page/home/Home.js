c15e66d786ff7c20e662833091c45db91dfcd943
Nicolae Postica <nicolae.postica2@gmail.com>
2021-03-25 18:43:57 +0200

    change channel playlist link

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -3,8 +3,7 @@ import "./home-styles.css";
 import {SidebarRight} from "./SidebarRight";
 
 const Home = () => (
-  <section id="page-3268"
-           className="qw-mainsection qw-page-section qw-top30 post-3268 page type-page status-publish hentry">
+  <section className="qw-mainsection qw-page-section qw-top30 page type-page status-publish hentry">
     <div className="container  qw-bottom30">
       <div className="row">
         <div className="col s12 m12 l8 ">
@@ -19,44 +18,44 @@ const Home = () => (
                 <div>
                   <h5>VOCAL TRANCE</h5>
                   <div>
-                    <a href="http://radio.vocaltrance.fm/vocaltrance_320.m3u">Vocal Trance 320</a><br/>
-                    <a href="http://radio.vocaltrance.fm/vocaltrance_256.m3u">Vocal Trance 256</a><br/>
-                    <a href="http://radio.vocaltrance.fm/vocaltrance_128.m3u">Vocal Trance 128</a>
+                    <a href="https://vocaltrance.fm/radio/vocaltrance_320.m3u">Vocal Trance 320</a><br/>
+                    <a href="https://vocaltrance.fm/radio/vocaltrance_256.m3u">Vocal Trance 256</a><br/>
+                    <a href="https://vocaltrance.fm/radio/vocaltrance_128.m3u">Vocal Trance 128</a>
                   </div>
                   <div/>
                 </div>
                 <div>
                   <h5>MELODIC PROGRESSIVE HOUSE</h5>
                   <div>
-                    <a href="http://radio.vocaltrance.fm/positive_320.m3u">Melodic Progressive House 320</a><br/>
-                    <a href="http://radio.vocaltrance.fm/positive_256.m3u">Melodic Progressive House 256</a><br/>
-                    <a href="http://radio.vocaltrance.fm/positive_128.m3u">Melodic Progressive House 128</a></div>
+                    <a href="https://vocaltrance.fm/radio/positive_320.m3u">Melodic Progressive House 320</a><br/>
+                    <a href="https://vocaltrance.fm/radio/positive_256.m3u">Melodic Progressive House 256</a><br/>
+                    <a href="https://vocaltrance.fm/radio/positive_128.m3u">Melodic Progressive House 128</a></div>
                   <div/>
                 </div>
                 <div>
                   <h5>VOCAL CHILLOUT</h5>
                   <div>
-                    <a href="http://radio.vocaltrance.fm/vocalchillout_320.m3u">Vocal Chillout 320</a><br/>
-                    <a href="http://radio.vocaltrance.fm/vocalchillout_256.m3u">Vocal Chillout 256</a><br/>
-                    <a href="http://radio.vocaltrance.fm/vocalchillout_128.m3u">Vocal Chillout 128</a>
+                    <a href="https://vocaltrance.fm/radio/vocalchillout_320.m3u">Vocal Chillout 320</a><br/>
+                    <a href="https://vocaltrance.fm/radio/vocalchillout_256.m3u">Vocal Chillout 256</a><br/>
+                    <a href="https://vocaltrance.fm/radio/vocalchillout_128.m3u">Vocal Chillout 128</a>
                   </div>
                   <div/>
                 </div>
                 <div>
                   <h5>UPLIFTING TRANCE</h5>
                   <div>
-                    <a href="http://radio.vocaltrance.fm/uplifting_320.m3u">Uplifting Trance 320</a><br/>
-                    <a href="http://radio.vocaltrance.fm/uplifting_256.m3u">Uplifting Trance 256</a><br/>
-                    <a href="http://radio.vocaltrance.fm/uplifting_128.m3u">Uplifting Trance 128</a>
+                    <a href="https://vocaltrance.fm/radio/uplifting_320.m3u">Uplifting Trance 320</a><br/>
+                    <a href="https://vocaltrance.fm/radio/uplifting_256.m3u">Uplifting Trance 256</a><br/>
+                    <a href="https://vocaltrance.fm/radio/uplifting_128.m3u">Uplifting Trance 128</a>
                   </div>
                   <div/>
                 </div>
                 <div>
                   <h5>DEEP VOCAL HOUSE</h5>
                   <div>
-                    <a href="http://radio.vocaltrance.fm/deep_320.m3u">Deep Vocal House 320</a><br/>
-                    <a href="http://radio.vocaltrance.fm/deep_256.m3u">Deep Vocal House 256</a><br/>
-                    <a href="http://radio.vocaltrance.fm/deep_128.m3u">Deep Vocal House 128</a>
+                    <a href="https://vocaltrance.fm/radio/deep_320.m3u">Deep Vocal House 320</a><br/>
+                    <a href="https://vocaltrance.fm/radio/deep_256.m3u">Deep Vocal House 256</a><br/>
+                    <a href="https://vocaltrance.fm/radio/deep_128.m3u">Deep Vocal House 128</a>
                   </div>
                   <div/>
                 </div>
